c0477c41a5759a700174596fec4a06f6ff8c5d81
sayan upadhyay <112376309+saiyan66@users.noreply.github.com>
2025-12-05 09:53:15 +0530

    Initial portfolio setup

---
+++ src/header.jsx
@@ -0,0 +1,12 @@
+
+function header() {
+    return (
+      <header>
+        <h1>Hi, I'm Sayan</h1>
+        <p>Welcome to my portfolio!</p>
+      </header>
+    );
+  }
+  
+  export default header;
+  
